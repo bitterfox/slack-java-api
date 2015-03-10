423b22105abb2cfe478cc9d77c6dd1441bcb8ee7
bitter_fox <bitterfoxc@gmail.com>
2015-03-10 20:06:06 +0900

    Add: TestSuite for api tests

---
+++ src/test/java/com/slack/api/ApiTestSuite.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.slack.api;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author bitter_fox
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+{
+    UsersInfoTest.class,
+    ChannelsCreate.class,
+    AuthTestErrorTest.class,
+    ChannelsRenameTest.class,
+    ChannelsJoinTest.class,
+    ChannelsLeaveTest.class
+})
+public class ApiTestSuite
+{
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+}
