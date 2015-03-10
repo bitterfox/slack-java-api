4c75bc9c42b690354ed3c5d00540c861bdceb428
bitter_fox <bitterfoxc@gmail.com>
2015-03-10 12:47:12 +0900

    Refactoring Unmarshaller

--- src/main/java/com/slack/data/impl/json/SlackJsonUnmarshallerImpl.java
+++ src/main/java/com/slack/data/impl/json/SlackJsonUnmarshallerImpl.java
@@ -12,6 +12,7 @@ import com.slack.data.Profile;
 import com.slack.data.Purpose;
 import com.slack.data.Topic;
 import com.slack.data.User;
+import com.slack.data.UserId;
 import com.slack.data.impl.ChannelImpl;
 import com.slack.data.impl.ProfileImpl;
 import com.slack.data.impl.PurposeImpl;
@@ -20,9 +21,13 @@ import com.slack.data.impl.UserIdImpl;
 import com.slack.data.impl.UserImpl;
 import com.slack.data.json.SlackJsonUnmarshaller;
 import com.slack.util.JsonUtil;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -86,31 +91,95 @@ public class SlackJsonUnmarshallerImpl implements SlackJsonUnmarshaller
         public static final String EMAIL = "email";
     }
 
+    private <T, U> void unmarshal(JsonObject jo, String key, BiFunction<JsonObject, String, T> getter, Consumer<U> consumer, Function<T, U> mapper)
+    {
+        consumer.accept(mapper.apply(getter.apply(jo, key)));
+    }
+
+    private <T, U> void unmarshalOpt(JsonObject jo, String key, BiFunction<JsonObject, String, T> getter, Consumer<U> consumer, Function<T, U> mapper)
+    {
+        if (jo.containsKey(key))
+        {
+            consumer.accept(mapper.apply(getter.apply(jo, key)));
+        }
+    }
+    private <T, U> void unmarshalOpt(JsonObject jo, String key, BiFunction<JsonObject, String, T> getter, Consumer<U> consumer, Function<T, U> mapper, Supplier<U> onEmpty)
+    {
+        if (jo.containsKey(key))
+        {
+            consumer.accept(mapper.apply(getter.apply(jo, key)));
+        }
+        else
+        {
+            consumer.accept(onEmpty.get());
+        }
+    }
+
+    private void unmarshalString(JsonObject jo, String key, Consumer<String> consumer)
+    {
+        this.unmarshal(jo, key, JsonObject::getString, consumer, Function.identity());
+    }
+    private <T> void unmarshalString(JsonObject jo, String key, Consumer<T> consumer, Function<String, T> mapper)
+    {
+        this.unmarshal(jo, key, JsonObject::getString, consumer, mapper);
+    }
+    private void unmarshalStringOpt(JsonObject jo, String key, Consumer<String> consumer)
+    {
+        this.unmarshalOpt(jo, key, JsonObject::getString, consumer, Function.identity());
+    }
+    private <T> void unmarshalStringOpt(JsonObject jo, String key, Consumer<T> consumer, Function<String, T> mapper)
+    {
+        this.unmarshalOpt(jo, key, JsonObject::getString, consumer, mapper);
+    }
+
+    private void unmarshalBoolean(JsonObject jo, String key, Consumer<Boolean> consumer)
+    {
+        this.unmarshal(jo, key, JsonObject::getBoolean, consumer, Function.identity());
+    }
+
+    private void unmarshalInt(JsonObject jo, String key, Consumer<Integer> consumer)
+    {
+        this.unmarshal(jo, key, JsonObject::getInt, consumer, Function.identity());
+    }
+    private <T> void unmarshalInt(JsonObject jo, String key, Consumer<T> consumer, Function<Integer, T> mapper)
+    {
+        this.unmarshal(jo, key, JsonObject::getInt, consumer, mapper);
+    }
+    private void unmarshalIntOpt(JsonObject jo, String key, Consumer<Integer> consumer, Supplier<Integer> onEmpty)
+    {
+        this.unmarshalOpt(jo, key, JsonObject::getInt, consumer, Function.identity(), onEmpty);
+    }
+
+    private <T> void unmarshalStringArray(JsonObject jo, String key, Consumer<List<T>> consumer, Function<String, T> mapper)
+    {
+        this.unmarshal(jo, key, JsonObject::getJsonArray, consumer,
+            o -> o.stream().map(JsonUtil::castToString).map(JsonString::getString)
+                .map(mapper)
+                .collect(Collectors.toList()));
+    }
+
+    private <T> void unmarshalObject(JsonObject jo, String key, Consumer<T> consumer, Function<JsonObject, T> mapper)
+    {
+        this.unmarshal(jo, key, JsonObject::getJsonObject, consumer, mapper);
+    }
+
     @Override
     public Channel asChannel(JsonObject jo)
     {
         ChannelImpl channel = new ChannelImpl();
 
-        channel.id(jo.getString(Names.ID));
-        channel.name(jo.getString(Names.NAME));
-        channel.isChannel(jo.getBoolean(Names.IS_CHANNEL));
-        channel.created(jo.getInt(Names.CREATED));
-        channel.creator(new UserIdImpl(jo.getString(Names.CREATOR)));
-        channel.isArchived(jo.getBoolean(Names.IS_ARCHIVED));
-        channel.isGeneral(jo.getBoolean(Names.IS_GENERAL));
-        channel.isMember(jo.getBoolean(Names.IS_MEMBER));
-        channel.members(jo.getJsonArray(Names.MEMBERS).stream().map(JsonUtil::castToString).map(JsonString::getString).map(UserIdImpl::new).collect(Collectors.toList()));
-        channel.topic(this.asTopic(jo.getJsonObject(Names.TOPIC)));
-        channel.purpose(this.asPurpose(jo.getJsonObject(Names.PURPOSE)));
-
-        if (jo.containsKey(Names.NUM_MEMBERS))
-        {
-            channel.numMembers(jo.getInt(Names.NUM_MEMBERS));
-        }
-        else
-        {
-            channel.numMembers(channel.members().size());
-        }
+        this.unmarshalString(jo,              Names.ID,          channel::id);
+        this.unmarshalString(jo,              Names.NAME,        channel::name);
+        this.unmarshalBoolean(jo,             Names.IS_CHANNEL,  channel::isChannel);
+        this.unmarshalInt(jo,                 Names.CREATED,     channel::created);
+        this.unmarshalString(jo,              Names.CREATOR,     channel::creator,     UserIdImpl::new);
+        this.unmarshalBoolean(jo,             Names.IS_ARCHIVED, channel::isArchived);
+        this.unmarshalBoolean(jo,             Names.IS_GENERAL,  channel::isGeneral);
+        this.unmarshalBoolean(jo,             Names.IS_MEMBER,   channel::isMember);
+        this.<UserId>unmarshalStringArray(jo, Names.MEMBERS,     channel::members,     UserIdImpl::new); // XXX
+        this.unmarshalObject(jo,              Names.TOPIC,       channel::topic,       this::asTopic);
+        this.unmarshalObject(jo,              Names.PURPOSE,     channel::purpose,     this::asPurpose);
+        this.unmarshalIntOpt(jo,              Names.NUM_MEMBERS, channel::numMembers,                    () -> channel.members().size());
 
         return channel;
     }
@@ -120,9 +189,9 @@ public class SlackJsonUnmarshallerImpl implements SlackJsonUnmarshaller
     {
         TopicImpl topic = new TopicImpl();
 
-        topic.value(jo.getString(Names.VALUE));
-        topic.creator(new UserIdImpl(jo.getString(Names.CREATOR)));
-        topic.lastSet(jo.getInt(Names.LAST_SET));
+        this.unmarshalString(jo, Names.VALUE,    topic::value);
+        this.unmarshalString(jo, Names.CREATOR,  topic::creator,  UserIdImpl::new);
+        this.unmarshalInt(jo,    Names.LAST_SET, topic::lastSet);
 
         return topic;
     }
@@ -132,9 +201,9 @@ public class SlackJsonUnmarshallerImpl implements SlackJsonUnmarshaller
     {
         PurposeImpl purpose = new PurposeImpl();
 
-        purpose.value(jo.getString(Names.VALUE));
-        purpose.creator(new UserIdImpl(jo.getString(Names.CREATOR)));
-        purpose.lastSet(jo.getInt(Names.LAST_SET));
+        this.unmarshalString(jo, Names.VALUE,    purpose::value);
+        this.unmarshalString(jo, Names.CREATOR,  purpose::creator,  UserIdImpl::new);
+        this.unmarshalInt(jo,    Names.LAST_SET, purpose::lastSet);
 
         return purpose;
     }
@@ -144,24 +213,25 @@ public class SlackJsonUnmarshallerImpl implements SlackJsonUnmarshaller
     {
         UserImpl user = new UserImpl();
 
-        user.id(new UserIdImpl(jo.getString(Names.ID)));
-        user.name(jo.getString(Names.NAME));
-        user.deleted(jo.getBoolean(Names.DELETED));
+        this.unmarshalString(jo,  Names.ID,                  user::id,                 UserIdImpl::new);
+        this.unmarshalString(jo,  Names.NAME,                user::name);
+        this.unmarshalBoolean(jo, Names.DELETED,             user::deleted);
         // TODO user.status
-        user.color(Color.web(jo.getString(Names.COLOR)));
-        user.realName(jo.getString(Names.REAL_NAME));
-        user.timeZone(ZoneId.of(jo.getString(Names.TZ))); // ignoreing tz_offset
-        user.timeZoneLabel(jo.getString(Names.TZ_LABEL));
+        this.unmarshalString(jo,  Names.COLOR,               user::color,              Color::web);
+        this.unmarshalString(jo,  Names.REAL_NAME,           user::realName);
+        this.unmarshalString(jo,  Names.TZ,                  user::timeZone,           ZoneId::of);
+        // ignore tz_offset
+        this.unmarshalString(jo,  Names.TZ_LABEL,            user::timeZoneLabel);
 
-        user.profile(this.asProfile(jo.getJsonObject(Names.PROFILE)));
+        this.unmarshalObject(jo,  Names.PROFILE,             user::profile,            this::asProfile);
 
-        user.isAdmin(jo.getBoolean(Names.IS_ADMIN));
-        user.isOwner(jo.getBoolean(Names.IS_OWNER));
-        user.isPrimaryOwner(jo.getBoolean(Names.IS_PRIMARY_OWNER));
-        user.isRestricted(jo.getBoolean(Names.IS_RESTRICTED));
-        user.isUltraRestricted(jo.getBoolean(Names.IS_ULTRA_RESTRICTED));
-        user.isBot(jo.getBoolean(Names.IS_BOT));
-        user.hasFiles(jo.getBoolean(Names.HAS_FILES));
+        this.unmarshalBoolean(jo, Names.IS_ADMIN,            user::isAdmin);
+        this.unmarshalBoolean(jo, Names.IS_OWNER,            user::isOwner);
+        this.unmarshalBoolean(jo, Names.IS_PRIMARY_OWNER,    user::isPrimaryOwner);
+        this.unmarshalBoolean(jo, Names.IS_RESTRICTED,       user::isRestricted);
+        this.unmarshalBoolean(jo, Names.IS_ULTRA_RESTRICTED, user::isUltraRestricted);
+        this.unmarshalBoolean(jo, Names.IS_BOT,              user::isBot);
+        this.unmarshalBoolean(jo, Names.HAS_FILES,           user::hasFiles);
 
         return user;
     }
@@ -173,38 +243,25 @@ public class SlackJsonUnmarshallerImpl implements SlackJsonUnmarshaller
 
         try
         {
-            profile.image(Profile.ImageSize.XS,
-                JsonUtil.getStringOpt(jo, Names.IMAGE_24)
-                    .map(URLUtil::unsafeCreate));
-            profile.image(Profile.ImageSize.S,
-                JsonUtil.getStringOpt(jo, Names.IMAGE_32)
-                    .map(URLUtil::unsafeCreate));
-            profile.image(Profile.ImageSize.M,
-                JsonUtil.getStringOpt(jo, Names.IMAGE_48)
-                    .map(URLUtil::unsafeCreate));
-            profile.image(Profile.ImageSize.L,
-                JsonUtil.getStringOpt(jo, Names.IMAGE_72)
-                    .map(URLUtil::unsafeCreate));
-            profile.image(Profile.ImageSize.XL,
-                JsonUtil.getStringOpt(jo, Names.IMAGE_192)
-                    .map(URLUtil::unsafeCreate));
-            profile.image(Profile.ImageSize.ORIGINAL,
-                JsonUtil.getStringOpt(jo, Names.IMAGE_ORIGINAL)
-                    .map(URLUtil::unsafeCreate));
+            BiConsumer<String, Profile.ImageSize> unmarshalImage = (name, size) ->
+                this.unmarshalStringOpt(jo, name,
+                    url -> profile.image(size, url), URLUtil::unsafeCreate);
 
-            JsonUtil.getStringOpt(jo, Names.FIRST_NAME)
-                .ifPresent(profile::firstName);
-            JsonUtil.getStringOpt(jo, Names.LAST_NAME)
-                .ifPresent(profile::lastName);
-            JsonUtil.getStringOpt(jo, Names.TITLE)
-                .ifPresent(profile::title);
-            JsonUtil.getStringOpt(jo, Names.SKYPE)
-                .ifPresent(profile::skype);
-            JsonUtil.getStringOpt(jo, Names.PHONE)
-                .ifPresent(profile::phone);
-            profile.realName(jo.getString(Names.REAL_NAME));
-            profile.realNameNormalized(jo.getString(Names.REAL_NAME_NORMALIZED));
-            profile.email(jo.getString(Names.EMAIL));
+            unmarshalImage.accept(      Names.IMAGE_24,       Profile.ImageSize.XS);
+            unmarshalImage.accept(      Names.IMAGE_32,       Profile.ImageSize.S);
+            unmarshalImage.accept(      Names.IMAGE_48,       Profile.ImageSize.M);
+            unmarshalImage.accept(      Names.IMAGE_72,       Profile.ImageSize.L);
+            unmarshalImage.accept(      Names.IMAGE_192,      Profile.ImageSize.XL);
+            unmarshalImage.accept(      Names.IMAGE_ORIGINAL, Profile.ImageSize.ORIGINAL);
+
+            this.unmarshalStringOpt(jo, Names.FIRST_NAME,           profile::firstName);
+            this.unmarshalStringOpt(jo, Names.LAST_NAME,            profile::lastName);
+            this.unmarshalStringOpt(jo, Names.TITLE,                profile::title);
+            this.unmarshalStringOpt(jo, Names.SKYPE,                profile::skype);
+            this.unmarshalStringOpt(jo, Names.PHONE,                profile::phone);
+            this.unmarshalString(jo,    Names.REAL_NAME,            profile::realName);
+            this.unmarshalString(jo,    Names.REAL_NAME_NORMALIZED, profile::realNameNormalized);
+            this.unmarshalString(jo,    Names.EMAIL,                profile::email);
         }
         catch (Exception e)
         {
